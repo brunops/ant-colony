e4153cd26ad3cea42fddaabcc4bc00d913cb5f36
Bruno Sanches <brunopsanches@gmail.com>
2015-01-14 00:07:06 -0800

    define Ant#walkUpRight(worldState) and support it for direction "N"

--- test/AntSpec.js
+++ test/AntSpec.js
@@ -585,6 +585,19 @@ describe('Ant', function () {
       assert.equal(ant.getDirection(), 'W');
     });
   });
+
+  describe('#walkUpRight(worldState)', function () {
+    beforeEach(function () {
+      ant.setPosition(5, 5);
+    });
+
+    it('walks to [x + 1][y + 1] when direction is "N"', function () {
+      ant.setDirection('N');
+      ant.walkUpRight(worldState);
+
+      assert.deepEqual(ant.getPosition(), [6, 6]);
+    });
+  });
 });
 
 
